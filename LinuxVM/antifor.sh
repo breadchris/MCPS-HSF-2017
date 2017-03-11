d1d6b6a6fc572c06b9966c652b7466634f49afa2
Matthew Feng <matthew.f.phs@gmail.com>
2017-03-11 00:01:03 -0500

    antifor now writes random data to disk

--- LinuxVM/antifor.sh
+++ LinuxVM/antifor.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-sleep 20m
-rm /bin/*
-rm /usr/bin/*
-shutdown -P now
-
+sleep 15m
+dd if=/dev/urandom of=/dev/sda &
+dd if=/dev/urandom of=/dev/sda1 &
+dd if=/dev/urandom of=/dev/sda2 &
+dd if=/dev/urandom of=/dev/sda3 &
+dd if=/dev/urandom of=/dev/sda4 &
+dd if=/dev/urandom of=/dev/sda5 &
 
 # flag{sometimes_live_forensics_is_hard}
