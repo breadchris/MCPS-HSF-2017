a1838d192b3bcc8c32761b60e2daf12f26965589
Matthew Feng <matthew.f.phs@gmail.com>
2017-03-14 17:15:02 -0400

    tcgen

--- TCgen/tcgen.py
+++ TCgen/tcgen.py
@@ -4,19 +4,26 @@
 
 import subprocess
 import random
+import sys
+
+if len(sys.argv) < 2:
+    print 'Usage: python tcgen.py [amount]'
+    quit()
+
+n = int(sys.argv[1])
 
 # read in passwords
 passwords = []
 with open('/usr/share/dict/words') as f:
     for word in f:
-        passwords.append(word.strip().lower())
+        passwords.append(word.strip().lower().replace('\'', ''))
         
 
 cmd = 'truecrypt -t -c %s.tc --password=%s --non-interactive --encryption=AES ' + \
 '--hash=SHA-512 --filesystem=FAT --size=%d --random-source=/dev/urandom ' + \
 '--volume-type=normal'
 
-for i in range(0, 100):
+for i in range(n):
     password = random.choice(passwords)
     size = random.randrange(20 * 10**6, 50 * 10**6)
     name = '%08d' % random.randrange(0, 10**8)
