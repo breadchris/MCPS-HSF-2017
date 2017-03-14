2ecd24a69d981f1744f6a3e95cf074676e5900ed
Matthew Feng <matthew.f.phs@gmail.com>
2017-03-14 12:18:10 -0400

    truecrypt generator

---
+++ DonniesSecretStash/suspicious-files/we-got-the-best-encryption-programs/scramble.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+# note: requires python2.7
+
+import math
+import random
+import hashlib
+import sys
+
+def process(i, j):
+	global DATA
+	DATA[i] = chr(ord(DATA[i]) ^ j)
+
+if len(sys.argv) < 3:
+	print 'Usage: python scramble.py [file] [key]'
+	quit()
+
+filename = '.'.join(sys.argv[1].split('.')[:-1])
+extension = '.' + sys.argv[1].split('.')[-1]
+key = sys.argv[2]
+seed = int(hashlib.sha512(key).hexdigest(), 16)
+
+random.seed(seed)
+
+DATA = list(open(filename + extension, 'rb').read())
+data_len = len(DATA)
+
+for i in xrange(data_len):
+	xor = random.randrange(255)
+	process(i, xor)
+
+outfile = ''
+if extension == '.enc':
+	outfile = 'decrypted-' + filename
+else:
+	outfile = filename + extension + '.enc'
+
+out = open(outfile, 'wb')
+out.write(''.join(DATA))
+out.close()
---
+++ TCgen/tcgen.py
@@ -0,0 +1,22 @@
+# random truecrypt volume generator
+
+import subprocess
+import random
+
+# read in passwords
+passwords = []
+with open('/usr/share/dict/words') as f:
+    for word in f:
+        passwords.append(word.strip().lower())
+        
+
+cmd = 'truecrypt -t -c %s.tc --password=%s --non-interactive --encryption=AES ' + \
+'--hash=SHA-512 --filesystem=FAT --size=%d --random-source=/dev/urandom ' + \
+'--volume-type=normal'
+
+for i in range(0, 100):
+    password = random.choice(passwords)
+    size = random.randrange(20 * 10**6, 50 * 10**6)
+    name = '%08d' % random.randrange(0, 10**8)
+    subprocess.call(cmd % (name, password, size), shell=True)
+    print '%s.tc created with password %s' % (name, password)
